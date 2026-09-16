ea70512fd6864002433a7c6167d0a5c19a6321ec
Umair Shah <nicefellow1234@gmail.com>
2026-09-16 08:26:27 +0500

    Polish Surah icon rendering and picker alignment

--- src/components/layout/font-readiness.tsx
+++ src/components/layout/font-readiness.tsx
@@ -7,11 +7,12 @@ export function FontReadiness() {
     let active = true;
 
     void Promise.allSettled([
-      document.fonts.load("40px surahnames"),
+      document.fonts.load('40px "surahnames"', "001"),
       document.fonts.load("43px IndoPak"),
       document.fonts.load("20px MehrNastaliq"),
-    ]).finally(() => {
-      if (active) document.documentElement.dataset.surahNamesFontReady = "true";
+    ]).then(([surahNamesResult]) => {
+      const surahNamesLoaded = surahNamesResult?.status === "fulfilled" && surahNamesResult.value.length > 0;
+      if (active && surahNamesLoaded) document.documentElement.dataset.surahNamesFontReady = "true";
     });
 
     return () => {
